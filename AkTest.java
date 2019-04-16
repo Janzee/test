6241a07b0c17e9ccf0f1163679a5ea7586c7ad62
Janzee <yangchangrui@aliyun.com>
2019-04-16 09:59:10 +0800

    Create AkTest.java

---
+++ AkTest.java
@@ -0,0 +1,12 @@
+package com.test.ak;
+
+import java.util.*;
+
+public class AkTest {
+
+  public static void main(String[] args) {
+    String accessKeyId = "LTAIy5ei1OrizDHU";
+    String accessKeySecret = "Urg2TKHygX0nTSaYYlZt9KbjeJzp74";
+    System.out.println(accessKeyId + ":" + accessKeySecret);
+  }
+}
