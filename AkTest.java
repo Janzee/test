a5f072e73add051f7af6add4985b4f33032406d8
Janzee <yangchangrui@aliyun.com>
2019-04-16 10:35:46 +0800

    Update AkTest.java

--- AkTest.java
+++ AkTest.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class AkTest {
 
   public static void main(String[] args) {
+    //just test
     String accessKeyId = "LTAIy5ei1OrizDHU";
     String accessKeySecret = "Urg2TKHygX0nTSaYYlZt9KbjeJzp74";
     System.out.println(accessKeyId + ":" + accessKeySecret);
