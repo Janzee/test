2baab63e9465fa207cdf9fee98a7563a3a825fd5
Janzee <yangchangrui@aliyun.com>
2019-06-11 19:04:20 +0800

    Create World.java

---
+++ World.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Test {
+
+  public void static main() {
+      String ak = "LTAINv6Rn3i5Z8tJ";//mock
+      String sk = "aaaabbbb";
+      System.out.println(ak + ":" + sk);
+  }
+
+}
