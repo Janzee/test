5b7cb55ff0e8bac1c432ff29f0d80c6362444b80
Janzee <yangchangrui@aliyun.com>
2019-06-11 19:06:44 +0800

    Update World.java

--- World.java
+++ World.java
@@ -4,7 +4,7 @@ public class Test {
 
   public void static main() {
       String ak = "LTAINv6Rn3i5Z8tJ";//mock
-      String sk = "aaaabbbb";
+      String sk = "aaaabbbb";//fake one
       System.out.println(ak + ":" + sk);
   }
 
