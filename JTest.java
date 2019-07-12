ac9b60f98ce8d85711bcdbe83d969484d4c9b187
Janzee <yangchangrui@aliyun.com>
2019-07-12 11:54:29 +0800

    Create JTest.java
    test

---
+++ JTest.java
@@ -0,0 +1,11 @@
+public class JTest {
+
+  private String accessKeyId;
+  private String accessKeySecret;
+  
+  public static void main() {
+      JTest t = new JTest();
+      t.accessKeyId = "LTAI3NdOBUfHmahG";
+      t.accessKeySecret = "xxxxxx";
+  }
+}
